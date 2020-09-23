ba77ccf3106a6ca989cde5397daecaab56212184
Pine Wu <octref@gmail.com>
2020-09-24 01:39:17 +0800

    (//ω//)

--- scripts/add-geocode.js
+++ scripts/add-geocode.js
@@ -3,27 +3,35 @@ const https = require('https')
 const path = require('path')
 const jsonSrc = require(path.resolve(__dirname, '../tmp/sponsors.json'))
 
-async function main() {
-	const items = jsonSrc.data.user.sponsorshipsAsMaintainer.edges.map((edge) => {
-		return edge.node.sponsorEntity
-	})
+/**
+ * users who cannot be mapped to correct location
+ */
+const specialCases = {
+  // ╮( ˘ ､ ˘ )╭
+  antfu: [-30.613889, 83.6625],
+  bencodezen: [-77.0366, 38.895]
+}
 
-	for (let item of items) {
-		if (item.location) {
-			// ╮( ˘ ､ ˘ )╭
-			if (typeof item.location === 'string' && item.location === 'undefined') {
-				item.geoCode = [-30.613889, 83.6625]
-				continue
-			}
+async function main() {
+  const items = jsonSrc.data.user.sponsorshipsAsMaintainer.edges.map((edge) => {
+    return edge.node.sponsorEntity
+  })
+
+  for (let item of items) {
+    if (item.location) {
+      if (specialCases[item.login]) {
+        item.geoCode = specialCases[item.login]
+        continue
+      }
 
       item.geoCode = await forwardGeocode(item.location)
       console.log(JSON.stringify(item))
-		}
-	}
+    }
+  }
 
-	fs.writeFileSync(
-		path.resolve(__dirname, '../src/sponsorsWithGeo.ts'),
-		`
+  fs.writeFileSync(
+    path.resolve(__dirname, '../src/sponsorsWithGeo.ts'),
+    `
 export interface Sponsor {
   login: string
   name: string | null
@@ -33,43 +41,43 @@ export interface Sponsor {
 }
   
 export const sponsorsData: Sponsor[] = ${JSON.stringify(items, null, 2)}`.trim()
-	)
+  )
 }
 
 async function getHttp(url) {
-	return new Promise((resolve, reject) => {
-		https
-			.get(url, { headers: { 'User-Agent': 'get-sponsors' } }, (res) => {
-				let rawData = ''
-				res.on('data', (chunk) => {
-					rawData += chunk
-				})
-				res.on('end', () => {
-					try {
-						const parsedData = JSON.parse(rawData)
-						resolve(parsedData)
-					} catch (e) {
-						reject(e)
-					}
-				})
-			})
-			.on('error', (e) => {
-				reject(e)
-			})
-	})
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, { headers: { 'User-Agent': 'get-sponsors' } }, (res) => {
+        let rawData = ''
+        res.on('data', (chunk) => {
+          rawData += chunk
+        })
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData)
+            resolve(parsedData)
+          } catch (e) {
+            reject(e)
+          }
+        })
+      })
+      .on('error', (e) => {
+        reject(e)
+      })
+  })
 }
 
 async function forwardGeocode(location) {
-	const reqUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
-		location
-	)}.json?access_token=${process.env.MAPBOX_KEY}`
+  const reqUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    location
+  )}.json?access_token=${process.env.MAPBOX_KEY}`
 
-	const data = await getHttp(reqUrl)
+  const data = await getHttp(reqUrl)
 
-	return data.features[0].center
+  return data.features[0].center
 }
 
 main().then(() => {
-	console.log('done')
-	process.exit()
+  console.log('done')
+  process.exit()
 })
